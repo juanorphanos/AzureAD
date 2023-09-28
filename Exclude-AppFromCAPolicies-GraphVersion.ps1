c6eb0e0dfe75dc79cb6c30ce583cd262a5bb73a7
Juan Pablo Orphanos <18380875+juanorphanos@users.noreply.github.com>
2023-09-28 16:23:39 -0300

    Add files via upload

---
+++ Exclude-AppFromCAPolicies-GraphVersion.ps1
@@ -0,0 +1,28 @@
+﻿
+
+param (
+    [string]$AppsIdFile,
+    [string]$PolicyId
+)
+
+
+#Connect-MgGraph -Scopes 'Policy.ReadWrite.ConditionalAccess', 'Policy.Read.All', 'Directory.Read.All', 'Agreement.Read.All', 'Application.Read.All'
+$appsids = get-content $AppsIdFile
+
+$params = @{
+  Conditions = @{
+    Applications=@{
+    ExcludeApplications = $appsids
+    }
+  }
+}
+
+
+try {
+Update-MgIdentityConditionalAccessPolicy -ConditionalAccessPolicyId $PolicyId -BodyParameter $params
+Write-Host "AppsId excluidos de la policy $($PolicyId)"
+}
+
+catch {
+Write-Host "Error al actualizar la policy $($PolicyId)"
+}
